99de221372468011f72a2013e7dbce14b1b90b75
Elizabeth Njoroge <njorogewambuielizabeth@gmail.com>
2023-03-31 16:56:37 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,23 @@
+def add(a,b):
+    answer = a+b
+    return answer
+
+def subtract(a,b):
+    answer = a-b
+    return answer
+
+def divide(a,b):
+    answer = a/b
+    return answer
+
+def multiply(a,b):
+    answer = a*b
+    return answer
+
+def reminder(a,b):
+    answer = a%b
+    return answer
+
+
+
+
